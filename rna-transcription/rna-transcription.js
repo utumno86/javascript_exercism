fed34739764be456de8cec79132bd5cfe808adcf
Michael Nash <michael@spatialnetworks.com>
2018-01-23 17:00:46 -0500

    updated javascript rna-transcription

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,11 +1,15 @@
-let Transcribe = function () {}
+let Transcribe = function() { };
 
-Transcribe.prototype.toRna = function (strand) {
-  let NewStrand = strand.split('');
-  let transcription = {'C': 'G', 'G': 'C', 'A': 'U', 'T': 'A'};
-  return NewStrand.map(function(element) {
-    return transcription[element];
-  }).join('');
+Transcribe.prototype.toRna = function(strand) {
+    let NewStrand = strand.split('');
+    let transcription = {'C': 'G', 'G': 'C', 'A': 'U', 'T': 'A'};
+    return NewStrand.map(function(element) {
+        if (transcription[element]) {
+            return transcription[element];
+        } else {
+            throw new Error('Invalid input');
+        }
+    }).join('');
 };
 
 module.exports = Transcribe;
