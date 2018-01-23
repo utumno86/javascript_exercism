289497afa72dcf3e25ee3613ba855dea640aa638
Michael Nash <michael@spatialnetworks.com>
2018-01-23 16:29:43 -0500

    updated javascript hamming

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,19 +1,19 @@
 /**
  * Calculates hamming distance between two DNA strands
 */
-function Hamming() {}
+function Hamming() { }
 Hamming.prototype.compute = function(string1, string2) {
-  let count = 0
-  if (string1.length !== string2.length) {
-    throw new Error('DNA strands must be of equal length.')
-  } else {
-    for (var i = 0; i < string1.length; i++) {
-      if (string1[i] !== string2[i]) {
-        count++
-      }
+    let count = 0;
+    if (string1.length !== string2.length) {
+        throw new Error('DNA strands must be of equal length.');
+    } else {
+        for (let i = 0; i < string1.length; i++) {
+            if (string1[i] !== string2[i]) {
+                count++;
+            }
+        }
     }
-  }
-  return count
-}
+    return count;
+};
 
-module.exports = Hamming
+module.exports = Hamming;
