c330db81b35b87ad85adadf6bcbab4d115d25098
Michael Nash <michael@spatialnetworks.com>
2018-01-23 16:42:43 -0500

    updated javascript pangram

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,22 +1,22 @@
 let Pangram = function(input) {
-  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  return {
-    isPangram: function() {
-      let pangram = true;
-      if (!input) {
-        pangram = false;
-      } else {
-        input = input.toLowerCase();
-        input = input.replace(/_/gi, '');
-        for (let i = 0; i < 26; i++) {
-          if (!input.includes(alphabet[i])) {
-            pangram = false;
-          }
-        }
-      }
-      return pangram;
-    }
-  }
-}
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    return {
+        isPangram: function() {
+            let pangram = true;
+            if (!input) {
+                pangram = false;
+            } else {
+                input = input.toLowerCase();
+                input = input.replace(/_/gi, '');
+                for (let i = 0; i < 26; i++) {
+                    if (!input.includes(alphabet[i])) {
+                        pangram = false;
+                    }
+                }
+            }
+            return pangram;
+        },
+    };
+};
 
 module.exports = Pangram;
--- pangram/pangram.spec.js
+++ pangram/pangram.spec.js
@@ -1,48 +1,48 @@
 var Pangram = require('./pangram');
 
-describe('Pangram()', function() {
-  it('empty sentence', function() {
-    let pangram = new Pangram('');
+describe('Pangram()', function ()  {
+  it('empty sentence', function () {
+    var pangram = new Pangram('');
     expect(pangram.isPangram()).toBe(false);
   });
 
-  it('pangram with only lower case', function() {
-    let pangram = new Pangram('the quick brown fox jumps over the lazy dog');
+  it('pangram with only lower case', function ()  {
+    var pangram = new Pangram('the quick brown fox jumps over the lazy dog');
     expect(pangram.isPangram()).toBe(true);
   });
 
-  it('missing character \'x\'', function() {
-    let pangram = new Pangram('a quick movement of the enemy will jeopardize five gunboats');
+  it("missing character 'x'", function ()  {
+    var pangram = new Pangram('a quick movement of the enemy will jeopardize five gunboats');
     expect(pangram.isPangram()).toBe(false);
   });
 
-  it('another missing character \'x\'', function() {
-    let pangram = new Pangram('the quick brown fish jumps over the lazy dog');
+  it("another missing character 'x'", function () {
+    var pangram = new Pangram('the quick brown fish jumps over the lazy dog');
     expect(pangram.isPangram()).toBe(false);
   });
 
-  it('pangram with underscores', function() {
-    let pangram = new Pangram('the_quick_brown_fox_jumps_over_the_lazy_dog');
+  it('pangram with underscores', function () {
+    var pangram = new Pangram('the_quick_brown_fox_jumps_over_the_lazy_dog');
     expect(pangram.isPangram()).toBe(true);
   });
 
-  it('pangram with numbers', function() {
-    let pangram = new Pangram('the 1 quick brown fox jumps over the 2 lazy dogs');
+  it('pangram with numbers', function () {
+    var pangram = new Pangram('the 1 quick brown fox jumps over the 2 lazy dogs');
     expect(pangram.isPangram()).toBe(true);
   });
 
-  it('missing letters replaced by numbers', function() {
-    let pangram = new Pangram('7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog');
+  it('missing letters replaced by numbers', function () {
+    var pangram = new Pangram('7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog');
     expect(pangram.isPangram()).toBe(false);
   });
 
-  it('pangram with mixed case and punctuation', function() {
-    let pangram = new Pangram('"Five quacking Zephyrs jolt my wax bed."');
+  it('pangram with mixed case and punctuation', function ()  {
+    var pangram = new Pangram('"Five quacking Zephyrs jolt my wax bed."');
     expect(pangram.isPangram()).toBe(true);
   });
 
-  it('pangram with non-ascii characters', function() {
-    let pangram = new Pangram('Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.');
+  it('pangram with non-ascii characters', function ()  {
+    var pangram = new Pangram('Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.');
     expect(pangram.isPangram()).toBe(true);
   });
 });
