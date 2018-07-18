5cc1d39045d88ac3b68cbb9767d84a4f318c8684
Michael Nash <michael@spatialnetworks.com>
2018-07-18 12:28:57 -0400

    restored simple cipher to kina where I was with it

--- simple-cipher/simple-cipher.spec.js
+++ simple-cipher/simple-cipher.spec.js
@@ -1,7 +1,7 @@
 var Cipher = require('./simple-cipher');
 
 describe('Random key generation', function () {
-  xit('generates keys at random', function () {
+  it('generates keys at random', function () {
     // Strictly speaking, this is difficult to test with 100% certainty.
     // But, if you have a generator that generates 100-character-long
     // strings of lowercase letters at random, the odds of two consecutively
@@ -17,18 +17,18 @@ describe('Random key cipher', function () {
     expect(cipher.key).toMatch(/^[a-z]+$/);
   });
 
-  xit('has a key that is at least 100 characters long', function () {
-    expect(cipher.key.length).toBeGreaterThanOrEqual(100);
+  it('has a key that is at least 100 characters long', function () {
+    expect(cipher.key.length).toBeGreaterThan(100);
   });
 
   // Here we take advantage of the fact that plaintext of "aaa..."
   // outputs the key. This is a critical problem with shift ciphers, some
   // characters will always output the key verbatim.
-  xit('can encode', function () {
+  it('can encode', function () {
     expect(cipher.encode('aaaaaaaaaa')).toEqual(cipher.key.substr(0, 10));
   });
 
-  xit('can decode', function () {
+  it('can decode', function () {
     expect(cipher.decode(cipher.key.substr(0, 10))).toEqual('aaaaaaaaaa');
   });
 
