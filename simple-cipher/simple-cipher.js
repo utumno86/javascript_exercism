d9235ba8d080ce9da285e9e2a3eaf0873df42001
Michael Nash <michael@spatialnetworks.com>
2018-09-24 13:20:26 -0400

    big helper function

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -3,60 +3,57 @@
 *@param {string} key the cipher key
 */
 function Cipher(key) {
-	if (key) {
-		if (key.toUpperCase() === key || !isNaN(key)) throw new Error('Bad key')
-		this.key = key
-	} else if (key === '') {
-		throw new Error('Bad key')
-	} else {
-		this.key = randomKey()
-	}
+  if (key) {
+    if (key.toUpperCase() === key || !isNaN(key)) throw new Error('Bad key')
+    this.key = key
+  } else if (key === '') {
+    throw new Error('Bad key')
+  } else {
+    this.key = randomKey()
+  }
 }
 
 Cipher.prototype.encode = function(input) {
-	return decodeEncode(input, true, this)
+  return decodeEncode(input, true, this)
 }
 
 Cipher.prototype.decode = function(input) {
-	let decoded = ''
-	if (input) {
-		let characters = input.split('')
-		for (i = 0; i < characters.length; i++) {
-			let characterCode = characters[i].charCodeAt(0) - 97
-			let keyCode = this.key[i].charCodeAt(0) - 97
-			let encodedCode = characterCode - keyCode
-			while (encodedCode < 0){
-				encodedCode += 26
-			}
-			decoded += String.fromCharCode(
-				encodedCode + 97
-			)
-		}
-	}
-	return decoded
+  return decodeEncode(input, false, this)
 }
 
 function decodeEncode(input, encode, cipher){
-	let encoded = ''
-	if (input && encode) {
-		let characters = input.split('');
-		for (i = 0; i < characters.length; i++) {
-			let keyIndex = i
-			while ( keyIndex >= cipher.key.length){
-				keyIndex -= cipher.key.length
-			}
-			let characterCode = characters[i].charCodeAt(0) - 97
-			let keyCode = cipher.key[keyIndex].charCodeAt(0) - 97
-			let encodedCode = characterCode + keyCode
-			while (encodedCode >= 26){
-				encodedCode -= 26
-			}
-			encoded += String.fromCharCode(
-				encodedCode + 97
-			)
-		}
-	}
-	return encoded
+  let encodedDecodedString = ''
+  if (input) {
+    let characters = input.split('');
+    for (i = 0; i < characters.length; i++) {
+      let keyIndex = i
+      if(encode){
+        while ( keyIndex >= cipher.key.length){
+          keyIndex -= cipher.key.length
+        }
+      }
+      let characterCode = characters[i].charCodeAt(0) - 97
+      let keyCode = cipher.key[keyIndex].charCodeAt(0) - 97
+      if (encode) {
+        let encodedCode = characterCode + keyCode
+        while (encodedCode >= 26){
+          encodedCode -= 26
+        }
+        encodedDecodedString += String.fromCharCode(
+          encodedCode + 97
+        )
+      } else {
+        let encodedCode = characterCode - keyCode
+        while (encodedCode < 0){
+          encodedCode += 26
+        }
+        encodedDecodedString += String.fromCharCode(
+          encodedCode + 97
+        )
+      }
+    }
+  }
+  return encodedDecodedString
 }
 
 /**
@@ -64,13 +61,13 @@ function decodeEncode(input, encode, cipher){
 *@return {string} cipher key
 */
 function randomKey() {
-	let key = ''
+  let key = ''
 
-	for (i = 0; i < 101; i++) {
-		key += String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-	}
+  for (i = 0; i < 101; i++) {
+    key += String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+  }
 
-	return key.toLowerCase()
+  return key.toLowerCase()
 }
 
 module.exports = Cipher;
