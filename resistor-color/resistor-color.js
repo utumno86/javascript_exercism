a6ab2c679b2870b2790363cd79bcdfbd6aa805b7
Michael Nash <michael@spatialnetworks.com>
2019-05-30 15:23:48 -0400

    update resistor-color

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,5 +1,3 @@
 export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
 
-export const colorCode = (color) => {
-  return COLORS.indexOf(color)
-}
+export const colorCode = color => COLORS.indexOf(color)
