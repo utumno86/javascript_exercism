c16c005922d8a622ad86b930fef194a863387d7a
Michael Nash <michael@spatialnetworks.com>
2019-07-17 12:27:02 -0400

    space age

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,8 +1,17 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
+export const age = (planet, seconds) => {
+  const orbital_period = ORBITAL_PERIODS[planet]
+  return parseFloat((seconds / orbital_period / EARTH_YEAR_SECONDS).toFixed(2))
 };
+
+const ORBITAL_PERIODS = {
+  earth: 1.00,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
+}
+
+const EARTH_YEAR_SECONDS = 31557600
